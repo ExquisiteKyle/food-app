922eb941a29e7cb806a79cf8106cd327b0af94b7
ExquisiteKyle <iKyleLim@gmail.com>
2020-01-29 10:45:42 +0800

    Lab4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
 import HomePage from "./components/HomePage/HomePage";
+import OrderPage from "./components/OrderPage/OrderPage";
+import AdminPage from "./components/AdminPage/AdminPage";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-        <HomePage />
-    ) 
+      <BrowserRouter>
+        <React.Fragment>
+          <Navbar />
+          <Switch>
+            <Route path="/orders" component={OrderPage} />
+            <Route path="/admin" component={AdminPage} />
+            <Route path="/" component={HomePage} />
+          </Switch>
+        </React.Fragment>
+      </BrowserRouter>
+    )
   }
 }
 
